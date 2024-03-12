064cf7315dd88df160db9a424a6ea9ca6f1795cc
Hoang Le <hoang.b.le.au@gmail.com>
2024-03-12 22:22:00 +1100

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-23.11";
+    }; 
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };  
+  };
+
+  # The `self` parameter is special, it refers to
+  # the attribute set returned by the `outputs` function itself.
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {    
+    nixosConfigurations.hle-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+	(import ./fenix/default.nix)
+		
+	# make home-manager as a module of nixos
+        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          # TODO replace ryan with your own username
+          home-manager.users.hle = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+        }
+      ];
+    };
+  };
+}
