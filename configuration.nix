52cac44e16a6ce3023a5c45bf70564ea8479c443
hle <hle@awesome>
2024-03-13 02:08:54 +1100

    fix: remove troublesome stuff

--- configuration.nix
+++ configuration.nix
@@ -106,6 +106,8 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     curl
+    #hyprland stuff
+    #wireplumber
     xdg-desktop-portal-hyprland
   ];
 
--- home.nix
+++ home.nix
@@ -122,7 +122,7 @@
     
     #hyprland stuff
     dolphin
-    wofi
+    wofi 
     # 
     
   ];
@@ -161,7 +161,7 @@
       env.TERM = "xterm-256color";
       font = {
         size = 12;
-        draw_bold_text_with_bright_colors = true;
+        #draw_bold_text_with_bright_colors = true;
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
