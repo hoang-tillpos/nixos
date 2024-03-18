0c5f824d70e5b827a3a22b46ecd534fd65783836
hoang <hoang.b.le.au@gmail.com>
2024-03-18 22:51:28 +1100

    feat: finally remove hardware bits

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
     ];
 
   nixpkgs.config.allowUnfree = true;
