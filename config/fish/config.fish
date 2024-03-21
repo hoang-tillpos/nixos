90a4cf1c21976b389883d67185e392d858e1f574
hoang <hoang.b.le.au@gmail.com>
2024-03-21 15:56:18 +1100

    feat: abbr vim

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# fish alias vim for nvim, and oldvim for vim
+abbr -a vim nvim
+abbr -a oldvim vim
+
+
+
 # start up starship for fish
 eval (starship init fish)
 direnv hook fish | source
+
