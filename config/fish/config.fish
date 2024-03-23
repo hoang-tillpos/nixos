a94962f7a7fd2a1e3595e5250d5eb6a3e08a5a3a
hoang <hoang.b.le.au@gmail.com>
2024-03-24 09:19:06 +1100

    feat: adding vscode config from this folder. change sleep

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias cursor="appimage-run ~/cursor.AppImage"
+
 # start up starship for fish
 eval (starship init fish)
 direnv hook fish | source
--- config/hypr/scripts/sleep.sh
+++ config/hypr/scripts/sleep.sh
@@ -1,5 +1,4 @@
 swayidle -w timeout 300 'hyprlock' \
-            timeout 600 'hyprctl dispatch dpms off' \
-            timeout 900 'systemctl suspend' \
-            resume 'hyprctl dispatch dpms on' \
-            before-sleep 'hyprlock' &
+	timeout 600 'hyprctl dispatch dpms off' \
+	resume 'hyprctl dispatch dpms on' \
+	before-sleep 'hyprlock' #timeout 900 'systemctl suspend' \
